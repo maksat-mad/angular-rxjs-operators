c609eaed89d1be459176465d8fef3d8f3938e47c
Maksat Madeniyetov <69766509+maksat-mad@users.noreply.github.com>
2023-08-02 16:25:06 +0600

    definition was added

--- src/app/components/debounce-time/debounce-time.component.ts
+++ src/app/components/debounce-time/debounce-time.component.ts
@@ -10,6 +10,12 @@ export class DebounceTimeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     const input = document.querySelector('input') as HTMLInputElement;
+
+    // Throttle time = can limit the number of clicks so only 1 goes through every second (prevents button spamming)
+
+    // Debounce time = can add a delay before each request goes through 
+    // (you press the button then nothing happens for 1 seconds, then your request goes through)
+    
     const observable = fromEvent(input, 'input');
     observable.pipe(
         map((event: any) => {
